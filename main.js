e75172f66d358cc119ade4236d78c168be7e527c
keerthi-balaji <keerthi.b.lv@gmail.com>
2025-08-01 00:23:38 +0000

    minor fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-document.getElementById('case1-card').addEventListener('click', function() {
-  window.location.href = 'cases/case1/case1.html';
+document.getElementById('case1-card').addEventListener('click', function () {
+  window.location.href = 'Cases/Case1/case1.html';
 });
 
-document.getElementById('case2-card').addEventListener('click', function() {
+document.getElementById('case2-card').addEventListener('click', function () {
   window.location.href = 'Cases/Case2/case2.html';
 });
